84a1ec30049733844cd72dc340daa627f24bc88b
trinita21 <trinita20221@iiitd.ac.in>
2022-02-04 14:10:24 +0530

    local storage for dark mode

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+const currentTheme = localStorage.getItem("theme");
+console.log(currentTheme);
+if (currentTheme == "dark") {
+  document.getElementById("toggleknop").innerHTML =
+    '<i class="fas fa-sun" style="color:#d8c658;"></i>';
+  document.body.classList.add("dark-mode");
+}
+
 function darkMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+  document.body.classList.toggle("dark-mode");
+
+  document.getElementById("toggleknop").innerHTML =
+    '<i class="fas fa-moon"</i>';
+
+  let theme = "light";
+  if (document.body.classList.contains("dark-mode")) {
+    document.getElementById("toggleknop").innerHTML =
+      '<i class="fas fa-sun"style="color:#d8c658;"></i>';
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
 }
